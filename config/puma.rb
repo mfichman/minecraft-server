1d4a00f892b272c159c0006ac89e30cdd44e6bc0
Matt Fichman <matt.fichman@gmail.com>
2020-05-01 13:07:21 -0400

    SSL

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -18,6 +18,18 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+ssl_port = ENV.fetch("SSL_PORT") { 443 }
+server_name = ENV.fetch("SERVER_NAME") { 'localhost' }
+
+key = Rails.application.credentials.dig(:ssl, server_name.to_sym, :key)
+cert = Rails.application.credentials.dig(:ssl, server_name.to_sym, :cert)
+
+if key.present?
+  File.write('config/key.pem', key)
+  File.write('config/cert.pem', cert)
+  ssl_bind '0.0.0.0', ssl_port, key: 'config/key.pem', cert: 'config/cert.pem', verify_mode: 'none'
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
