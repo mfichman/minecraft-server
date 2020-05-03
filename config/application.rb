13302fde19e937ece86791f3c805c7cb7018d68f
Matt Fichman <matt.fichman@gmail.com>
2020-05-03 12:07:01 -0400

    Fix sprockets

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+#require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,6 +27,9 @@ module MinecraftServer
 
     #config.active_job.queue_adapter = :sucker_punch
     config.active_job.queue_adapter = :sidekiq
+
+    # Disable sprockets
+    config.assets.enabled = false
 
     config.eager_load_paths << Rails.root.join('app', 'validators')
     config.eager_load_paths << Rails.root.join('lib')
