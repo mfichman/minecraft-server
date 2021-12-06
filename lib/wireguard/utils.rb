e34e4df906ac7c3c24ab18398b526d167b44a56a
Matt Fichman <matt.fichman@gmail.com>
2021-12-06 09:36:16 -0500

    Added conf sync

--- lib/wireguard/utils.rb
+++ lib/wireguard/utils.rb
@@ -9,9 +9,9 @@ module Wireguard
       File.write(conf_file, conf)
 
       if new_config
-        command = "wg syncconf wg0 <(wg-quick strip /etc/wireguard/wg0.conf)\n"
-      else
         command = "wg-quick down wg0; wg-quick up wg0\n"
+      else
+        command = "wg-quick up wg0; wg syncconf wg0 <(wg-quick strip /etc/wireguard/wg0.conf)\n"
       end
 
       container = Docker::Container.get('wireguard')
