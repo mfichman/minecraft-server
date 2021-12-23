d3ed0d43e213cc448e5ae2b6c990843d98cd1c01
Matt Fichman <matt.fichman@gmail.com>
2021-12-22 21:38:46 -0500

    iOS

---
+++ bin/gen-icons.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash 
+
+convert minecraft.svg -bordercolor white -border '20%' -resize 80x80   -negate apple-touch-icon-80x80.png
+convert minecraft.svg -bordercolor white -border '20%' -resize 152x152 -negate apple-touch-icon-152x152.png
+convert minecraft.svg -bordercolor white -border '20%' -resize 167x167 -negate apple-touch-icon-167x167.png
+convert minecraft.svg -bordercolor white -border '20%' -resize 180x180 -negate apple-touch-icon-180x180.png
