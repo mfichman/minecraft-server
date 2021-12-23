2426bb8b7baf955e726ea70dcedd9c7b92095b20
Matt Fichman <matt.fichman@gmail.com>
2021-12-23 10:03:37 -0500

    Colorize icons

--- bin/gen-icons.sh
+++ bin/gen-icons.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-convert minecraft.svg -bordercolor white -border '20%' -resize 80x80   -negate apple-touch-icon-80x80.png
-convert minecraft.svg -bordercolor white -border '20%' -resize 152x152 -negate apple-touch-icon-152x152.png
-convert minecraft.svg -bordercolor white -border '20%' -resize 167x167 -negate apple-touch-icon-167x167.png
-convert minecraft.svg -bordercolor white -border '20%' -resize 180x180 -negate apple-touch-icon-180x180.png
+convert app/assets/images/minecraft.svg -bordercolor white -border '20%' -negate -fill 'hsl(155, 90%, 37%)' -opaque '#000000' -colorize 20% -resize 80x80   app/assets/images/apple-touch-icon-80x80.png
+convert app/assets/images/minecraft.svg -bordercolor white -border '20%' -negate -fill 'hsl(155, 90%, 37%)' -opaque '#000000' -colorize 20% -resize 152x152 app/assets/images/apple-touch-icon-152x152.png
+convert app/assets/images/minecraft.svg -bordercolor white -border '20%' -negate -fill 'hsl(155, 90%, 37%)' -opaque '#000000' -colorize 20% -resize 167x167 app/assets/images/apple-touch-icon-167x167.png
+convert app/assets/images/minecraft.svg -bordercolor white -border '20%' -negate -fill 'hsl(155, 90%, 37%)' -opaque '#000000' -colorize 20% -resize 180x180 app/assets/images/apple-touch-icon-180x180.png
